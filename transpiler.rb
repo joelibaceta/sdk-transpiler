6ce22b036aa7897c231b2a2b7b35f09c2e156cd5
Joel Ibaceta <mail@joelibaceta.com>
2017-03-16 13:59:44 -0300

    transpiler improved

--- transpiler.rb
+++ transpiler.rb
@@ -1,31 +1,59 @@
 require 'yaml'
 require 'pp'
 
-def transpile(code: "", from: "", to: "")
-   
+def transpile(code: "", from: "", to: "") 
   
   origin = YAML.load_file("#{File.dirname(__FILE__)}/descriptors/#{from}.yml")
   destiny = YAML.load_file("#{File.dirname(__FILE__)}/descriptors/#{to}.yml")
   
-  origin["expression_patterns"].each do |key, expression| 
-    #p expression["pattern"]
-    pattern = expression["pattern"]
-    
-    #match = code.match(pattern)
-    #captures = Hash[ match.names.zip( match.captures ) ]  
-    final_format = destiny["expression_patterns"][key]["format"]
-    
-    code.scan(pattern) do |i|
-      match = (i.join).match(pattern) 
-      _format = final_format
-      captures = Hash[ match.names.zip( match.captures ) ] 
-      _format.scan(/\{\{(.*?)\}\}/m).each do |variable|  
-        _format=_format.gsub("{{#{variable[0]}}}", captures[variable[0]]) 
-      end 
-      code = code.gsub(i.join, _format)
-    end  
-  end   
-  return code
+  parse_code(code, origin, destiny)
 end
+
+def parse_code(code_block, origin_languague_parser, final_languague_parser)
+
+  origin_languague_parser.each do |key, expression|   
+    
+    pattern = expression["pattern"]
+    final_structure = final_languague_parser[key]["format"]
+
+
+    #puts "TRYING WITH #{key}"
+
+    code_block.scan(pattern) do |sentence|
+      sentence_structure = final_structure
+      match = (sentence.join).match(pattern)
+
+      #puts "code #{sentence.join} \n under match #{match.captures rescue ""} \n for #{pattern} \n as #{key}"
+
+      if match != nil
+        captures = Hash[match.names.zip( match.captures)]
+
+        #puts "String: #{sentence.join} MATCH: #{match.captures} : Pattern: #{pattern}"
+
+        sentence_structure.scan(/\{\{(.*?)\}\}/m).flatten.each do |tag|
+          if (final_languague_parser[key][tag] != nil)
+            replace  = parse_code(captures[tag], origin_languague_parser[key][tag], final_languague_parser[key][tag])
+            sentence_structure=sentence_structure.gsub("{{#{tag}}}", replace) #puts "We are gonna replace {{#{tag}}} in #{sentence_structure} with #{replace}"
+          else
+            #puts "We are gonna replace {{#{tag}}} in #{sentence_structure} with #{captures[tag]}"
+            sentence_structure=sentence_structure.gsub("{{#{tag}}}", captures[tag])
+          end
+        end
+        code_block = code_block.gsub(sentence.join, sentence_structure)
+      else
+
+      end
+    end  
+    
+  end
+
+  return code_block
+end
+
+
+#match = code.match(pattern)
+#captures = Hash[ match.names.zip( match.captures ) ] 
+#p "String: #{i.join} MATCH: #{match} : Pattern: #{pattern}" 
+ 
 
   
